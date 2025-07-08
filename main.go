34c3895d28007a4ca371cffd833f42d04f66684e
ArseniyK1 <qwertototo2332@gmail.com>
2025-07-08 17:13:50 +0300

    fix: improve server startup logging
    - Added a log statement to indicate when the server is running.
    - Removed redundant log statement from the error handling section in main.go.

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	router := http.NewServeMux()
 	routers.Router(router)
 
+	fmt.Println("Server is running on http://localhost:8080")
 	err := http.ListenAndServe(":8080", middleware.Middleware(router))
+
 	if err != nil {
 		fmt.Println("Server is not running", err)
-	} else {
-		fmt.Println("Server is running on http://localhost:8080")
 	}
 }
