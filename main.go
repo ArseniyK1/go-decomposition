f6cfa3b5a2023c6f0594772d8cd3e845ca71270f
ArseniyK1 <qwertototo2332@gmail.com>
2025-07-09 20:38:34 +0300

    chore: update dependencies and configuration
    - Added new dependencies in go.mod for PostgreSQL and logging.
    - Created go.sum to manage dependency versions.
    - Updated .gitignore to include environment files and output directories.
    - Added repomix configuration file for output settings.
    - Introduced a new router file for handling routes in a dedicated package.
    - Enhanced main.go to establish a database connection and improved error handling.

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"decomposition/internal/app/handlers/routers"
+	"context"
 	"decomposition/internal/app/middleware"
+	"decomposition/internal/app/routers"
 	"fmt"
 	"net/http"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
@@ -12,10 +15,19 @@ func main() {
 	router := http.NewServeMux()
 	routers.Router(router)
 
-	fmt.Println("Server is running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", middleware.Middleware(router))
-
+	conn, err := pgx.Connect(context.Background(), "postgres://posgtres:root@localhost:5432/dbname")
 	if err != nil {
+		fmt.Println("Failed to connect to database:", err)
+		return
+	}
+	defer conn.Close(context.Background())
+	fmt.Println("Connected to database!")
+
+	fmt.Println("Server is running on http://localhost:8080")
+
+	error := http.ListenAndServe(":8080", middleware.Middleware(router))
+
+	if error != nil {
 		fmt.Println("Server is not running", err)
 	}
 }
